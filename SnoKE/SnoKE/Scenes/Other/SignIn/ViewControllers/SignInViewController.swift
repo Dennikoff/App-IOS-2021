e4c9ccd0d64af5eeb1153035e71b49b97c2b1671
Ilya Buldin <evilya0607@gmail.com>
2021-10-21 16:41:11 +0300

    sign in dev init commit

--- SnoKE/SnoKE/Scenes/Other/SignIn/ViewControllers/SignInViewController.swift
+++ SnoKE/SnoKE/Scenes/Other/SignIn/ViewControllers/SignInViewController.swift
@@ -6,24 +6,88 @@
 //
 
 import UIKit
+import EasyPeasy
 
 class SignInViewController: UIViewController {
-
+    
+    private let buttonEnter: UIButton = {
+        let button = UIButton()
+        button.setTitle("Войти", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        //button.set(.darkGray, for: .normal)
+        button.backgroundColor = .orange
+        button.layer.cornerRadius = 12
+        button.isHidden = false
+        return button
+    }()
+    
+    private let buttonSignUp: UIButton = {
+        let button = UIButton()
+        button.setTitle("Зарегистрироваться", for: .normal)
+        button.setTitleColor(.gray, for: .normal)
+        //button.set(.darkGray, for: .normal)
+        button.backgroundColor = .none
+        button.isHidden = false
+        return button
+    }()
+    
+    private let label:UILabel = {
+        let label = UILabel()
+        label.text = "Кто вы?\nНемедленно\nназовитесь"
+        label.numberOfLines = 0
+        label.layer.cornerRadius = 12
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize:50)
+        return label
+    }()
+    
+    private let TextFieldUserName: UITextField = {
+        let txt = UITextField()
+        txt.placeholder = "Почта"
+        //let txt = UITextField(frame: CGRect(origin: CGPoint(x: 10, y:20), size:CGSize(width: 40, height: 10)), primaryAction: nil)
+        return txt
+    }()
+    
+    private let TextFieldPassword: UITextField = {
+        let txt = UITextField()
+        txt.placeholder = "Пароль"
+        //let txt = UITextField(frame: CGRect(origin: CGPoint(x: 10, y:20), size:CGSize(width: 40, height: 10)), primaryAction: nil)
+        return txt
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        [buttonEnter, label, TextFieldUserName,TextFieldPassword,buttonSignUp].forEach{
+            view.addSubview($0)}
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setlayout()
     }
-    */
 
+    private func setlayout(){
+        label.easy.layout(
+            CenterX(-30),
+            CenterY(-100)
+        )
+        TextFieldUserName.easy.layout(
+            Left(50),
+            Top(20).to(label)
+        )
+        TextFieldPassword.easy.layout(
+            Left(50),
+            Top(20).to(TextFieldUserName)
+        )
+        buttonEnter.easy.layout(
+            CenterX(),
+            Top(20).to(TextFieldPassword),
+            Size(CGSize(width: 100, height: 50))
+        )
+        buttonSignUp.easy.layout(
+            CenterX(),
+            Bottom(20)
+        )
+    }
 }
