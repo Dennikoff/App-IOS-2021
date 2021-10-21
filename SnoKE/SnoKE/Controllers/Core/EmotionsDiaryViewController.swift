fa8f51bc453bd9bda41116b5e02efa17f5de53c7
Ilya Buldin <evilya0607@gmail.com>
2021-10-21 16:39:16 +0300

    dev init commit

--- SnoKE/SnoKE/Controllers/Core/EmotionsDiaryViewController.swift
+++ SnoKE/SnoKE/Controllers/Core/EmotionsDiaryViewController.swift
@@ -11,7 +11,8 @@ class EmotionsDiaryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.backgroundColor = .red
         
     }
     
