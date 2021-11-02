7f6653580948df54ed6d17779d804e8fe0c68437
Денис Харке <hda007@campus.mephi.ru>
2021-11-02 17:40:40 +0300

    add containers and button
    add containers for texfields and button that show password

--- SnoKE/SnoKE/Scenes/Other/SignIn/View/SignInView.swift
+++ SnoKE/SnoKE/Scenes/Other/SignIn/View/SignInView.swift
@@ -14,8 +14,10 @@ protocol SignInViewDelegate:AnyObject{
 }
 
 class SignInView: UIView {
-
+    var k = 0.0
+    
     weak var delegate: SignInViewDelegate?
+    
     private let buttonEnter: UIButton = {
         $0.setTitle("Войти", for: .normal)
         $0.setTitleColor(.white, for: .normal)
@@ -35,6 +37,29 @@ class SignInView: UIView {
         return $0
     }(UIButton())
     
+    private let buttonShowPassword: UIButton = {
+        let bt = UIButton()
+        $0.setBackgroundImage(UIImage(systemName: "eye"), for: .normal)
+        $0.addTarget(self, action: #selector(showPassword), for: .touchUpInside)
+        return $0
+    }(UIButton())
+    
+    private var emailContainer: UIView = {
+        //$0.layer.borderWidth = 0.5
+        $0.layer.cornerRadius = 8
+        $0.backgroundColor = .none
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIView())
+    
+    private var passwordContainer: UIView = {
+        //$0.layer.borderWidth = 0.5
+        $0.layer.cornerRadius = 8
+        $0.backgroundColor = .none
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIView())
+    
     private let badMessageLabel:UILabel = {
         $0.text = "Не узнаю вас, попробуйте ввести данные снова"
         $0.numberOfLines = 0
@@ -53,18 +78,19 @@ class SignInView: UIView {
         return $0
     }(UILabel())
     
-    let label:UILabel = {
-        $0.text = "Кто вы?\nНемедленно\nназовитесь"
+    lazy var label:UILabel = {
+        $0.text = "Кто вы?\nНемедленно назовитесь"
         $0.numberOfLines = 0
         $0.layer.cornerRadius = 12
         $0.textColor = .black
-        $0.font = UIFont.systemFont(ofSize:50)
+        $0.font = UIFont.systemFont(ofSize:k / 20)
         return $0
     }(UILabel())
     
     private let textFieldUserName: UITextField = {
         $0.placeholder = "Почта"
         //let txt = UITextField(frame: CGRect(origin: CGPoint(x: 10, y:20), size:CGSize(width: 40, height: 10)), primaryAction: nil)
+        $0.autocapitalizationType = .none
         return $0
     }(UITextField())
     
@@ -72,12 +98,16 @@ class SignInView: UIView {
         $0.placeholder = "Пароль"
         //let txt = UITextField(frame: CGRect(origin: CGPoint(x: 10, y:20), size:CGSize(width: 40, height: 10)), primaryAction: nil)
         $0.isSecureTextEntry = true
+        $0.autocapitalizationType = .none
         return $0
     }(UITextField())
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        [label,buttonEnter,buttonSignUp, textFieldPassword, textFieldUserName, badMessageLabel, goodMessageLabel].forEach {
+        emailContainer.addSubview(textFieldUserName)
+        passwordContainer.addSubview(textFieldPassword)
+        passwordContainer.addSubview(buttonShowPassword)
+        [label,buttonEnter,buttonSignUp, emailContainer, passwordContainer, badMessageLabel, goodMessageLabel].forEach {
             self.addSubview($0)
         }
     }
@@ -89,6 +119,8 @@ class SignInView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         setlayout()
+        k = self.width
+        label.font = UIFont.systemFont(ofSize:k / 8.56)
     }
     
     private func setlayout(){
@@ -102,15 +134,36 @@ class SignInView: UIView {
         )
         label.easy.layout(
             Left(40),
+            Right(50),
             CenterY(-100)
         )
+        emailContainer.easy.layout(
+            Left(40),
+            Right(40),
+            Top(20).to(badMessageLabel),
+            Height(35)
+        )
         textFieldUserName.easy.layout(
-            Left(50),
-            Top(20).to(badMessageLabel)
+            Left(10),
+            Right(10),
+            CenterY()
+        )
+        passwordContainer.easy.layout(
+            Left(40),
+            Right(40),
+            Top(10).to(emailContainer),
+            Height(35)
         )
         textFieldPassword.easy.layout(
-            Left(50),
-            Top(20).to(textFieldUserName)
+            Left(10),
+            Right(10),
+            CenterY()
+        )
+        buttonShowPassword.easy.layout(
+            Right(5),
+            Width(25),
+            Height(20),
+            CenterY()
         )
         buttonEnter.easy.layout(
             CenterX(),
@@ -119,16 +172,27 @@ class SignInView: UIView {
         )
         buttonSignUp.easy.layout(
             CenterX(),
-            Bottom(20)
+            Bottom(UIApplication.shared.windows.first?.safeAreaInsets.bottom ?? .zero)
         )
     }
     @objc
     func enterButtonTapped(){
+        //guard let сделать
         let mail = textFieldUserName.text
         let password = textFieldPassword.text
         delegate?.enterButtonTapped(mail!, password!)
     }
-    
+    @objc
+    func showPassword(){
+        if buttonShowPassword.backgroundImage(for: .normal) == UIImage(systemName: "eye") {
+            textFieldPassword.isSecureTextEntry = false
+            buttonShowPassword.setBackgroundImage(UIImage(systemName: "eye.slash"), for: .normal)
+        }
+        else{
+            textFieldPassword.isSecureTextEntry = true
+            buttonShowPassword.setBackgroundImage(UIImage(systemName: "eye"), for: .normal)
+        }
+    }
     @objc
     func signUpButtonTapped(){
         delegate?.signUpButtonTapped()
